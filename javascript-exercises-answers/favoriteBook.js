267b466840aa017c21ad14943fd0223fc4394733
jcastle <joseph.castle@gsa.gov>
2020-06-02 09:55:40 -0400

    Setup w/ first exercise

---
+++ javascript-exercises-answers/favoriteBook.js
@@ -0,0 +1,25 @@
+// TODO: define addFavoriteBook(..) function
+function addFavoriteBook(bookName) {
+	if (!bookName.includes("Great")) {
+		favoriteBooks.push(bookName);
+	}
+}
+
+// TODO: define printFavoriteBooks() function
+function printFavoriteBooks() {
+    console.log(`Favorite books: ${favoriteBooks.length}`);
+    for (let bookName of favoriteBooks) {
+        console.log(`${bookName}`);
+    }
+}
+
+var favoriteBooks = [];
+
+addFavoriteBook("A Song of Ice and Fire");
+addFavoriteBook("The Great Gatsby");
+addFavoriteBook("Crime & Punishment");
+addFavoriteBook("Great Expectations");
+addFavoriteBook("You Don't Know JS");
+
+// TODO: print out favorite books
+printFavoriteBooks();
